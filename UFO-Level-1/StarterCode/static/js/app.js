579c9e2e8bc3990f4bf4c1c4ee139569ce4c4cf8
Sebastian Echeverri <sebas1996e@gmail.com>
2020-01-27 06:52:59 -0500

    finish

--- UFO-Level-1/StarterCode/static/js/app.js
+++ UFO-Level-1/StarterCode/static/js/app.js
@@ -1,4 +1,50 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// select the tbdoy 
+tbody = d3.select('tbody')
+
+// select the submit nbutton 
+var submit = d3.select("#filter-btn")
+
+// function that dispaus UFO sightings 
+function init(data1) {
+    tbody.text("")
+    data.forEach(function(sightings){
+        new_tr = tbody.append("tr")
+        Object.entries(sightings).forEach(function([key,value]){
+            new_td = new_tr.append("td").text(value)
+        })
+
+    })
+}
+init(tableData)
+
+// do the filter 
+
+submit.on("click", function(){
+    // prevent from page refreshing 
+    d3.event.preventDefault();
+
+    //clearing the previous table data
+    tbody.html("");
+
+    //get the data entered in text box 
+    //var inputElement = d3.select(#filtertype).focus()
+    var inputValue = d3.select("#datetime").node().value;
+    //filter Data based on input value
+    var filteredData = tableData.filter(sightings => sightings.datetime === inputValue);
+    console.log(filteredData);
+
+    //displaying the data
+    filteredData.forEach(function(sightings){
+        new_tr = tbody.append("tr")
+        Object.entries(sightings).forEach(function([key,value]){
+            new_td = new_tr.append("td").text(value)
+        })
+
+    })
+
+
+})
+
